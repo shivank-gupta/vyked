c1131945ea79520135cfe9575c6a47f392a4c75e
him3010 <himanshu.jain1@1mg.com>
2017-11-22 18:52:05 +0530

    http decorator content_type check added for request.post and request.json method

--- vyked/decorators/http.py
+++ vyked/decorators/http.py
@@ -49,19 +49,25 @@ def get_decorated_fun(method, path, required_params, timeout):
                                                 server_type='http', time_taken=0, process_time_taken=0)
                         return Response(status=400, content_type='application/json', body=json.dumps(res_d).encode())
 
+
+                _logger = logging.getLogger()
+
                 # Support for multi request body encodings
                 req = args[0]
                 try:
-                    yield from req.json()
+                    content_type = req.content_type
+                    _logger.debug('Content-Type %s for method %s', content_type, func.__name__)
+                    if str(content_type) in ['application/json']:
+                        req.post = req.json
                 except:
                     pass
-                else:
-                    req.post = req.json
+
+
                 t1 = time.time()
                 tp1 = time.process_time()
                 wrapped_func = func
                 success = True
-                _logger = logging.getLogger()
+
                 api_timeout = _http_timeout
 
                 if valid_timeout(timeout):
